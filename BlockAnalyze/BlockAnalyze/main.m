6618af3328711d38b6f341956d2f15aa93cfa6f2
jiaxw32 <jiaxw32@gmail.com>
2021-03-30 09:48:01 +0800

    Block 类型

--- BlockAnalyze/BlockAnalyze/main.m
+++ BlockAnalyze/BlockAnalyze/main.m
@@ -6,8 +6,45 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
 
 typedef void(^blk_t)(void);
+
+static Class _BlockClass(void);
+
+// Block 继承链：
+// __NSMallocBlock -> NSBlock -> NSObject
+// __NSStackBlock -> NSBlock -> NSObject
+// __NSGlobalBlock__ -> __NSGlobalBlock -> NSBlock -> NSObject
+
+/// 获取 block 基类 NSBlock，摘自 FBRetainCycleDetector 源码
+static Class _BlockClass() {
+  static dispatch_once_t onceToken;
+  static Class blockClass;
+  dispatch_once(&onceToken, ^{
+    void (^testBlock)(void) = [^{} copy];
+    blockClass = [testBlock class];
+      NSLog(@"%@", blockClass);
+    while(class_getSuperclass(blockClass) && class_getSuperclass(blockClass) != [NSObject class]) {
+      blockClass = class_getSuperclass(blockClass);
+        NSLog(@"%@", blockClass);
+    }
+  });
+  return blockClass;
+}
+
+
+/// 判断一个对象是否为 block
+/// @param object 待检测对象指针
+BOOL CheckObjectIsBlock(void *object) {
+  Class blockClass = _BlockClass();
+  
+  Class candidate = object_getClass((__bridge id)object);
+  return [candidate isSubclassOfClass:blockClass];
+    
+}
+
+#pragma mark - MYClass
 
 @interface MYClass : NSObject{
     NSString *_name;
@@ -76,6 +113,8 @@ typedef void(^blk_t)(void);
 
 @end
 
+#pragma mark - test
+
 int g_var1 = 0;
 
 static int g_var2 = 0;
@@ -138,6 +177,57 @@ void testBlockCaptureVar(){
     
 }
 
+NSArray* testBlockArray01(){
+    /*
+     可变参数，只会 copy 第一个，第一个 block 存储在堆上，其他 block 存储在栈上。作用域结束调用 release，就触发崩溃。
+     
+     + (instancetype)arrayWithObjects:(ObjectType)firstObj, ... ;
+     
+     第一个类型是明确的，显式指定转为对象，编译器才会调用 block 的 copy/retain。变参是非显式指定的，所以调用前编译不知道他要转成对象，调用时 NSArray 不知道是个 stackblock。
+     变参的类型默认是 int 的，所 objc_msgsend 直接禁用了变参版本
+     */
+    int i = 0;
+    NSArray *arr = [NSArray arrayWithObjects:
+                    ^(){ printf("%d", i); }, // __NSMallocBlock 分配在堆上
+                    ^(){ printf("%d", i); }, // __NSStackBlock__ 分配在栈上
+                    ^(){ printf("%d", i); }, // __NSStackBlock__ 分配在栈上
+                    nil];
+    return arr;
+}
+
+NSArray* testBlockArray02(){
+    int i = 0;
+    NSArray *arr = [NSArray arrayWithObjects:
+                    [^(){ printf("%d", i); } copy], // __NSMallocBlock 分配在堆上
+                    [^(){ printf("%d", i); } copy], // __NSMallocBlock 分配在堆上
+                    [^(){ printf("%d", i); } copy], // __NSMallocBlock 分配在堆上
+                    nil];
+    return arr;
+}
+
+NSArray* testBlockArray03(){
+    int i = 0;
+    
+    NSMutableArray *arr = [[NSMutableArray alloc] init];
+    [arr addObject:^(){ printf("%d", i); }]; // __NSMallocBlock 分配在堆上
+    [arr addObject:^(){ printf("%d", i); }]; // __NSMallocBlock 分配在堆上
+    [arr addObject:^(){ printf("%d", i); }]; // __NSMallocBlock 分配在堆上
+    
+    return arr;
+}
+
+NSArray* testBlockArray04(){
+    NSArray *arr = [NSArray arrayWithObjects:
+                    ^(){ printf("Objective-C"); }, // __NSGlobalBlock
+                    ^(){ printf("C++"); }, // __NSGlobalBlock
+                    ^(){ printf("Swift"); }, // __NSGlobalBlock
+                    nil];
+    return arr;
+}
+
+
+#pragma mark - main
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
@@ -149,6 +239,19 @@ int main(int argc, const char * argv[]) {
             MYClass *obj = [[MYClass alloc] init];
             NSLog(@"%@", obj);
         }
+        
+//        {
+//            NSArray *arr01 = testBlockArray01();
+//            NSLog(@"%@", arr01);
+//            // 作用域结束时触发 EXC_BAD_ACCESS 异常
+//        }
+        
+        testBlockArray03();
+        
+        testBlockArray04();
+
+        printf("Hello world!");
+        
     }
     return 0;
 }
