332146913208c6100a384fce27cb152a6a167058
jiaxw32 <jiaxw32@gmail.com>
2022-01-12 11:29:08 +0800

    swift tuple demo.

---
+++ SwiftNote.playground/Pages/TuplePage.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+//: [Next](@next)
+
+
+//: # Swift Tuple
+
+//: ## 初始化 Tuple
+
+//: - 省略标签
+let tuple0 = ("John", 35, true)
+print(type(of: tuple0))
+print(tuple0)
+
+//: - 使用标签
+var tuple1 = (name: "John", age: 35, gender: true)
+print(tuple1)
+
+//: ## 访问元素中的元素
+
+//: - 通过索引访问元素
+let element0 = tuple1.0
+print(element0)
+
+//: - 使用标签访问元素
+let element1 = tuple1.age
+print(element1)
+
+//: ## 修改元组元素
+
+print("The tuple before update: \(tuple1)")
+tuple1.age = 60
+print("The tuple after update: \(tuple1)")
+
+//: ## 作为函数返回值
+
+func getATuple() -> (String, Int, Bool) {
+    return ("John", 35, true)
+}
+
+// 使用下划线忽略元组值
+let (name, age, _) = getATuple()
+print("\(name)'s age was \(age)")
