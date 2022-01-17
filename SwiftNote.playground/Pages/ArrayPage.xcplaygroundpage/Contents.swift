ba8c2597dedb77e1813784aa3dae0bcbaf44a8d8
jiaxw32 <jiaxw32@gmail.com>
2022-01-17 17:51:44 +0800

    swift array study.

--- SwiftNote.playground/Pages/ArrayPage.xcplaygroundpage/Contents.swift
+++ SwiftNote.playground/Pages/ArrayPage.xcplaygroundpage/Contents.swift
@@ -50,8 +50,23 @@ for prime in primes {
 }
 print("")
 
+//: - range
+
+print("iterating over array elements using range:", terminator: " ")
+for idx in 0..<primes.count {
+    print(primes[idx], separator: "", terminator: " ")
+}
+print("")
+
+print("iterating over array elements using indices:", terminator: " ")
+for idx in primes.indices {
+    print(primes[idx], separator: "", terminator: " ")
+}
+print("")
+
 //: - while
 var idx = 0
+print("iterating over array elements using while:", terminator: " ")
 while idx < primes.count {
     print(primes[idx], separator: "", terminator: " ")
     idx += 1
@@ -59,6 +74,8 @@ while idx < primes.count {
 print("")
 
 //: - forEach block
+
+print("iterating over array elements using forEach block:", terminator: " ")
 primes.forEach { prime in
     print("\(prime)", separator: "", terminator: " ")
 }
@@ -66,6 +83,15 @@ print("")
 
 primes.forEach {
     print($0, separator: "", terminator: " ")
+}
+print("")
+
+//: enumerated
+
+print("iterating over array elements using enumerated method:", terminator: " ")
+//use the enumerated method to convert our array into a sequence containing tuples that pair each index with its associated element
+for (idx, prime) in primes.enumerated() {
+    print("(\(idx), \(prime))", separator: "", terminator: " ")
 }
 print("")
 
@@ -142,5 +168,138 @@ if fruits_0 == fruits_1 {
 } else {
     print("Two arrays are not equal.")
 }
+
+//: ## 在数组中查找元素索引
+
+//: - 查找元素索引
+
+let names = ["apple", "banana", "cherry", "mango"]
+let ele = "pear"
+
+if let idx = names.firstIndex(of: ele) {
+    print("Index of \(ele) is \(idx)")
+} else {
+    print("Element is not present in the array.")
+}
+
+//: - 查找元素最小值
+
+let nums = [6, 4 ,2, 8, 10]
+
+if let result = nums.min() {
+    print("Minimum num is: \(result)")
+} else {
+    print("Array is emptry")
+}
+
+let arr10 = ["apple", "banana", "cherry", "mango"]
+if let result = arr10.min(by: { element0, element1 in element0.count < element1.count }) {
+    print("Minimum element: \(result)")
+}
+
+//: - 查找最大值
+
+if let num = nums.max() {
+    print("Maximum num is: \(num)")
+} else {
+    print("Array is empty.")
+}
+
+if let result = arr10.max(by: { $0.count < $1.count}) {
+    print("Maximum element: \(result)")
+}
+
+//: ## 数组转换
+
+//: - 数组添加元素
+
+var fruits_01 = ["apple", "banana", "cherry"]
+var anotherFruit = "mango"
+fruits_01.append(anotherFruit)
+print(fruits_01)
+
+//: - 数组添加数组
+
+var fruits_02 = ["apple", "banana", "cherry"]
+var moreFruits = ["mango", "guava"]
+fruits_02.append(contentsOf: moreFruits)
+print(fruits_02)
+
+//: - 连接两个数组
+
+var arr01: [Int] = [22, 54]
+var arr02: [Int] = [35, 68]
+
+var new_array = arr01 + arr02
+print(new_array)
+
+//: - 删除数组中前 N 个元素
+
+print("Original Array: \(fruits_02)")
+var result = fruits_02.dropFirst(2)
+print("Drop first N result Array: \(result)")
+
+//: - 删除数组中第一个元素
+
+print("Original Array: \(fruits_02)")
+result = fruits_02.dropFirst()
+print("Drop first result Array: \(result)")
+
+//: - 删除数组中最后 N 个元素
+
+print("Original Array: \(fruits_02)")
+result = fruits_02.dropLast(2)
+print("Drop last N result Array: \(result)")
+
+result = fruits_02.dropLast(10)
+print("Another drop last N result Array: \(result)")
+
+
+result = fruits_02.dropLast()
+print("Drop last result Array: \(result)")
+
+//: - 在数组指定位置插入元素
+
+var fruits_03 = ["apple", "banana", "cherry", "mango"]
+var fruitGuava = "guava"
+fruits_03.insert(fruitGuava, at: 0)
+print(fruits_03)
+
+//Swift/Array.swift:405: Fatal error: Array index is out of range
+//fruits_03.insert(fruitGuava, at: 10)
+
+//: - 在数组指定公位置插入另外一个数组
+
+var fruits_04 = ["apple", "banana", "cherry"]
+var moreFruits_04 = ["guava", "mango"]
+fruits_04.insert(contentsOf: moreFruits_04, at: 0)
+print(fruits_04)
+
+//: - 删除数组中指定位置元素
+
+var numbers = [2, 3, 5, 7, 11]
+numbers.remove(at: 1)
+numbers.forEach { print($0) }
+
+//: - 指定条件删除数组中元素
+
+numbers.removeAll(where: { $0 > 5})
+print(numbers)
+
+//: - 删除数组中所有元素
+
+numbers.removeAll()
+print(numbers)
+
+//: - 反转数组
+
+var anotherNumbers = [2, 3, 5, 7, 11]
+print("Original array: \(anotherNumbers)")
+
+let reverseResult: [Int] = anotherNumbers.reversed()
+print("Reversed Array: \(reverseResult)")
+
+anotherNumbers.reverse()
+print("Reverse Array: \(anotherNumbers)")
 
 //: [Next](@next)
