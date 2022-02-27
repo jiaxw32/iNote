9ec1579884fa2640a60d3475aa829a0cf390be78
jiaxw32 <jiaxw32@gmail.com>
2022-02-27 22:01:44 +0800

    swift memory

--- SwiftNote.playground/Pages/UnsafePointerPage.xcplaygroundpage/Contents.swift
+++ SwiftNote.playground/Pages/UnsafePointerPage.xcplaygroundpage/Contents.swift
@@ -4,6 +4,43 @@ import Foundation
 import Darwin
 
 
+func bytes<T>(of value: T) -> [UInt8] {
+    var value = value
+    let size = MemoryLayout<T>.size
+    return withUnsafePointer(to: &value) {
+        $0.withMemoryRebound(to: UInt8.self, capacity: size) {
+            Array(UnsafeBufferPointer(start: $0, count: size))
+        }
+    }
+}
+
+func hexString<Seq: Sequence>
+         (bytes: Seq, limit: Int? = nil, separator: String = " ")
+         -> String
+    where Seq.Iterator.Element == UInt8 {
+         let spacesInterval = 8
+         var result = ""
+         for (index, byte) in bytes.enumerated() {
+             if let limit = limit, index >= limit {
+                 result.append("...")
+                 break
+             }
+             if index > 0 && index % spacesInterval == 0 {
+                 result.append(separator)
+             }
+             result.append(String(format: "%02x", byte))
+         }
+         return result
+    }
+
+do {
+    let x = 0x0102030405060708
+    print(bytes(of: x))
+    print(hexString(bytes: bytes(of: x)))
+    print(bytes(of: 42))
+    print(hexString(bytes: bytes(of: 42)))
+}
+
 let count = 2
 let stride = MemoryLayout<Int>.stride
 let alignment = MemoryLayout<Int>.alignment
@@ -27,8 +64,13 @@ do {
     
     let ptrBuffer = UnsafeRawBufferPointer(start: ptr, count: byteCount)
     for (index, byte) in ptrBuffer.enumerated() {
-        print("byte \(index): \(byte)")
+        if index > 0, index % 8 == 0{
+            print(String(format:" %02x", byte), terminator: "")
+        } else {
+            print(String(format:"%02x", byte), terminator: "")
+        }
     }
+    print("")
 }
 
 //: ## Typed Pointer
@@ -50,7 +92,10 @@ do {
     
     let bufferPointer = UnsafeBufferPointer(start: pointer, count: count)
     for (index, value) in bufferPointer.enumerated() {
-        print("value \(index): \(value)")
+        print(value, terminator: " ")
+        if index == count - 1 {
+            print("")
+        }
     }
 }
 
@@ -61,9 +106,10 @@ do {
     
     let typedPtr = rawPointer.bindMemory(to: Int.self, capacity: count)
     typedPtr.initialize(repeating: 0, count: count)
-//    defer {
-//        typedPtr.deinitialize(count: count)
-//    }
+    defer {
+        typedPtr.deinitialize(count: count)
+        rawPointer.deallocate()
+    }
     
     typedPtr.pointee = 42
     (typedPtr + 1).pointee = 6
@@ -72,11 +118,11 @@ do {
     
     let bufferPtr = UnsafeBufferPointer(start: typedPtr, count: count)
     for (index, value) in bufferPtr.enumerated() {
-        print("value \(index): \(value)")
+        print(value, terminator: " ")
+        if index == count - 1 {
+            print("")
+        }
     }
-    
-    typedPtr.deinitialize(count: count)
-    rawPointer.deallocate()
 }
 
 //: ## Get the bytes of an instance
@@ -85,9 +131,10 @@ do {
 
 do {
     var i = 32
+    print("bytes of Int value: ", terminator: "")
     withUnsafeBytes(of: &i) { bytes in
         for byte in bytes {
-            print(String(format:"%02X", byte), terminator: " ")
+            print(String(format:"%02X", byte), terminator: "")
         }
         print("")
     }
@@ -98,12 +145,19 @@ do {
     }
     
     var puppy1 = Puppy(age: 3, isTrained: true)
+    print("bytes of The struct instannce puppy1: ", terminator: "")
     withUnsafeBytes(of: &puppy1) { bytes in
-        for byte in bytes {
-            print(String(format:"%02X", byte), terminator: " ")
+        for (index, byte) in bytes.enumerated() {
+            if index > 0, index % 8 == 0 {
+                print(String(format:" %02X", byte), terminator: "")
+            } else {
+                print(String(format:"%02X", byte), terminator: "")
+            }
         }
         print("")
     }
+    
+    print("bytes of puppy1: \(hexString(bytes: bytes(of: puppy1)))")
     
     struct AnotherPuppy {
         let isTrained: Bool
@@ -111,12 +165,19 @@ do {
     }
     
     var puppy2 = AnotherPuppy(isTrained: true, age: 4)
+    print("bytes of puppy2: ", terminator: "")
     withUnsafeBytes(of: &puppy2) { bytes in
-        for byte in bytes {
-            print(String(format:"%02X", byte), terminator: " ")
+        for (index, byte) in bytes.enumerated() {
+            if index > 0, index % 8 == 0 {
+                print(String(format:" %02X", byte), terminator: "")
+            } else {
+                print(String(format:"%02X", byte), terminator: "")
+            }
         }
         print("")
     }
+    
+    print("bytes of puppy2: \(hexString(bytes: bytes(of: puppy2)))")
 }
 
 //: ### Class
@@ -131,6 +192,8 @@ do {
     print("instance size:\t \(class_getInstanceSize(EmptyClass.self))") // 16
     
     var obj = EmptyClass()
+    
+    print(hexString(bytes: bytes(of: obj)))
     
     let ptr = UnsafeRawPointer(bitPattern: unsafeBitCast(obj, to: UInt.self))!
     print("The pointer value: \(ptr)")
@@ -161,5 +224,4 @@ do {
     }
     print("")
 }
-
 //: [Next](@next)
