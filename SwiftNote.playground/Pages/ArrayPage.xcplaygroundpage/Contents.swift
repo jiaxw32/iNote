a32f920feac899caccd74ffc5e769f54ff24fbaf
jiaxw32 <jiaxw32@gmail.com>
2022-03-02 10:55:52 +0800

    swift array

--- SwiftNote.playground/Pages/ArrayPage.xcplaygroundpage/Contents.swift
+++ SwiftNote.playground/Pages/ArrayPage.xcplaygroundpage/Contents.swift
@@ -6,47 +6,59 @@ import Foundation
 
 //: ## 创建数组
 
+print("### Initialization of the array\n")
+
 //: - 创建空数组
 
-var emptyArray0 = [Int]()
-emptyArray0.append(0)
-emptyArray0.append(1)
-print(emptyArray0)
+do {
+    var emptyArray0 = [Int]()
+    emptyArray0.append(0)
+    emptyArray0.append(1)
+    print(emptyArray0)
 
-//var emptyArray1 = [String]()
-//var fruits: [String] = []
-//print(fruits)
+    var fruits: [String] = []
+    fruits.append("apple")
+    print(fruits)
+}
+
 
 //: - 创建具有初始值的数组
 
-var countries = ["USA", "India", "Russia", "China", "Japan"]
-print(countries)
-
+do {
+    let countries = ["USA", "India", "Russia", "China", "Japan"]
+    print(countries)
+}
 //: - 指定大小创建数组
 
 // Array(repeating: defaultValue, count: specificSize)
-var arr0 = Array(repeating: "", count: 8)
-print(arr0)
 
-var arr1 = [Int](repeating: 0, count: 4)
-print(arr1)
+do {
+    let arr0 = Array(repeating: "", count: 8)
+    print(arr0)
 
+    let arr1 = [Int](repeating: 0, count: 4)
+    print(arr1)
+}
 
 //: - 创建具有不同数据类型的数组
 
-var arr2 = ["a", 1, "b", 2, "c"] as [Any]
-print(type(of: arr2), arr2)
+do {
+    let arr0 = ["a", 1, "b", 2, "c"] as [Any]
+    print(type(of: arr0), arr0)
 
-var arr3: [Any] = ["a", 1, "b", 2]
-print(arr3)
+    let arr1: [Any] = ["a", 1, "b", 2]
+    print(arr1)
+}
 
 //: ## 遍历数组
+
+print("\n### Traversing the array\n")
 
 var primes: [Int] = [2, 3, 5, 7, 11]
 
 //: - for in
 for prime in primes {
-    print(prime, separator: "", terminator: " ")
+    print(prime, terminator: " ")
 }
 print("")
 
@@ -54,13 +66,13 @@ print("")
 
 print("iterating over array elements using range:", terminator: " ")
 for idx in 0..<primes.count {
-    print(primes[idx], separator: "", terminator: " ")
+    print(primes[idx], terminator: " ")
 }
 print("")
 
 print("iterating over array elements using indices:", terminator: " ")
 for idx in primes.indices {
-    print(primes[idx], separator: "", terminator: " ")
+    print(primes[idx], terminator: " ")
 }
 print("")
 
@@ -68,7 +80,7 @@ print("")
 var idx = 0
 print("iterating over array elements using while:", terminator: " ")
 while idx < primes.count {
-    print(primes[idx], separator: "", terminator: " ")
+    print(primes[idx], terminator: " ")
     idx += 1
 }
 print("")
@@ -77,12 +89,13 @@ print("")
 
 print("iterating over array elements using forEach block:", terminator: " ")
 primes.forEach { prime in
-    print("\(prime)", separator: "", terminator: " ")
+    print("\(prime)", terminator: " ")
 }
 print("")
 
+print("iterating over array elements using forEach block:", terminator: " ")
 primes.forEach {
-    print($0, separator: "", terminator: " ")
+    print($0, terminator: " ")
 }
 print("")
 
@@ -97,6 +110,7 @@ print("")
 
 //: ## 访问数组元素
 
+print("\n### Accessing the array elements\n")
 
 //: - 通过索引访问数组元素
 
@@ -142,44 +156,55 @@ fruits.forEach { fruit in
 
 //: ## 数组检测
 
+print("\n### Check the array\n")
+
 //: - Check Array is Empty
 
-let myArray0: [Int] = [0, 1, 2, 3]
-print(myArray0.isEmpty)
+let myArray: [Int] = [0, 1, 2, 3]
+print("Is the array empty?", myArray.isEmpty)
 
-let myArray1: [String] = []
-print(myArray1.isEmpty)
+do {
+    let arr: [String] = []
+    print("Is the array empty?", arr.isEmpty)
+}
 
 //: - Check if specific element is present
 
 let num0: Int = 5
-print("Is elelment \(num0) present in array? \(myArray0.contains(num0))")
+print("Is elelment \(num0) present in array? \(myArray.contains(num0))")
 
 let num1: Int = 3
-print("Is elelment \(num1) present in array? \(myArray0.contains(num1))")
+print("Is elelment \(num1) present in array? \(myArray.contains(num1))")
 
 //: - 检测两个数组是否相等
 
-let fruits_0: [String] = ["apple", "mango", "cherry"]
-let fruits_1: [String] = ["apple", "avacado", "cherry"]
+do {
+    let arr0: [String] = ["apple", "mango", "cherry"]
+    let arr1: [String] = ["apple", "avacado", "cherry"]
 
-if fruits_0 == fruits_1 {
-    print("Two arrays are equal.")
-} else {
-    print("Two arrays are not equal.")
+    if arr0 == arr1 {
+        print("Two arrays are equal.")
+    } else {
+        print("Two arrays are not equal.")
+    }
 }
+
 
 //: ## 在数组中查找元素索引
 
+print("\n### Search elements in a array\n")
+
 //: - 查找元素索引
 
-let names = ["apple", "banana", "cherry", "mango"]
-let ele = "pear"
+do {
+    let fruits = ["apple", "banana", "cherry", "mango"]
+    let ele = "pear"
 
-if let idx = names.firstIndex(of: ele) {
-    print("Index of \(ele) is \(idx)")
-} else {
-    print("Element is not present in the array.")
+    if let idx = fruits.firstIndex(of: ele) {
+        print("Index of \(ele) is \(idx)")
+    } else {
+        print("'pear' is not present in the array.")
+    }
 }
 
 //: - 查找元素最小值
@@ -192,9 +217,11 @@ if let result = nums.min() {
     print("Array is emptry")
 }
 
-let arr10 = ["apple", "banana", "cherry", "mango"]
-if let result = arr10.min(by: { element0, element1 in element0.count < element1.count }) {
-    print("Minimum element: \(result)")
+do {
+    let fruits = ["apple", "banana", "cherry", "mango"]
+    if let result = fruits.min(by: { element0, element1 in element0.count < element1.count }) {
+        print("Minimum element: \(result)")
+    }
 }
 
 //: - 查找最大值
@@ -205,81 +232,104 @@ if let num = nums.max() {
     print("Array is empty.")
 }
 
-if let result = arr10.max(by: { $0.count < $1.count}) {
-    print("Maximum element: \(result)")
+do {
+    let fruits = ["apple", "banana", "cherry", "mango"]
+    if let result = fruits.max(by: { $0.count < $1.count}) {
+        print("Maximum element: \(result)")
+    }
 }
 
 //: ## 数组转换
 
+print("\n### Transformation of the array\n")
+
 //: - 数组添加元素
 
-var fruits_01 = ["apple", "banana", "cherry"]
-var anotherFruit = "mango"
-fruits_01.append(anotherFruit)
-print(fruits_01)
+do {
+    var fruits = ["apple", "banana", "cherry"]
+    let anotherFruit = "mango"
+    fruits.append(anotherFruit)
+    print(fruits)
+}
 
 //: - 数组添加数组
 
-var fruits_02 = ["apple", "banana", "cherry"]
-var moreFruits = ["mango", "guava"]
-fruits_02.append(contentsOf: moreFruits)
-print(fruits_02)
+do {
+    var fruits = ["apple", "banana", "cherry"]
+    let moreFruits = ["mango", "guava"]
+    fruits.append(contentsOf: moreFruits)
+    print(fruits)
+}
 
 //: - 连接两个数组
 
-var arr01: [Int] = [22, 54]
-var arr02: [Int] = [35, 68]
+do {
+    let arr01: [Int] = [22, 54]
+    let arr02: [Int] = [35, 68]
 
-var new_array = arr01 + arr02
-print(new_array)
+    let new_array = arr01 + arr02
+    print(new_array)
+}
 
 //: - 删除数组中前 N 个元素
 
-print("Original Array: \(fruits_02)")
-var result = fruits_02.dropFirst(2)
-print("Drop first N result Array: \(result)")
+do {
+    let fruits = ["apple", "banana", "cherry"]
+    print("Original Array: \(fruits)")
+    let result = fruits.dropFirst(2)
+    print("Drop first N result Array: \(result)")
+}
 
 //: - 删除数组中第一个元素
 
-print("Original Array: \(fruits_02)")
-result = fruits_02.dropFirst()
-print("Drop first result Array: \(result)")
+do {
+    let fruits = ["apple", "banana", "cherry"]
+    print("Original Array: \(fruits)")
+    let result = fruits.dropFirst()
+    print("Drop first result Array: \(result)")
+}
 
 //: - 删除数组中最后 N 个元素
 
-print("Original Array: \(fruits_02)")
-result = fruits_02.dropLast(2)
-print("Drop last N result Array: \(result)")
+do {
+    let fruits = ["apple", "banana", "cherry"]
+    print("Original Array: \(fruits)")
+    var result = fruits.dropLast(2)
+    print("Drop last N result Array: \(result)")
 
-result = fruits_02.dropLast(10)
-print("Another drop last N result Array: \(result)")
+    result = fruits.dropLast(10)
+    print("Another drop last N result Array: \(result)")
 
-
-result = fruits_02.dropLast()
-print("Drop last result Array: \(result)")
+    result = fruits.dropLast()
+    print("Drop last result Array: \(result)")
+}
 
 //: - 在数组指定位置插入元素
 
-var fruits_03 = ["apple", "banana", "cherry", "mango"]
-var fruitGuava = "guava"
-fruits_03.insert(fruitGuava, at: 0)
-print(fruits_03)
-
-//Swift/Array.swift:405: Fatal error: Array index is out of range
-//fruits_03.insert(fruitGuava, at: 10)
+do {
+    var fruits = ["apple", "banana", "cherry", "mango"]
+    let fruitGuava = "guava"
+    fruits.insert(fruitGuava, at: 0)
+    print(fruits)
+    //Swift/Array.swift:405: Fatal error: Array index is out of range
+    //fruits_03.insert(fruitGuava, at: 10)
+}
 
 //: - 在数组指定公位置插入另外一个数组
 
-var fruits_04 = ["apple", "banana", "cherry"]
-var moreFruits_04 = ["guava", "mango"]
-fruits_04.insert(contentsOf: moreFruits_04, at: 0)
-print(fruits_04)
+do {
+    var fruits = ["apple", "banana", "cherry"]
+    let moreFruits = ["guava", "mango"]
+    fruits.insert(contentsOf: moreFruits, at: 0)
+    print(fruits)
+}
 
 //: - 删除数组中指定位置元素
 
 var numbers = [2, 3, 5, 7, 11]
 numbers.remove(at: 1)
-numbers.forEach { print($0) }
+numbers.forEach { print($0, terminator: " ") }
+print("")
 
 //: - 指定条件删除数组中元素
 
@@ -293,13 +343,15 @@ print(numbers)
 
 //: - 反转数组
 
-var anotherNumbers = [2, 3, 5, 7, 11]
-print("Original array: \(anotherNumbers)")
+do {
+    var anotherNumbers = [2, 3, 5, 7, 11]
+    print("Original array: \(anotherNumbers)")
 
-let reverseResult: [Int] = anotherNumbers.reversed()
-print("Reversed Array: \(reverseResult)")
+    let reverseResult: [Int] = anotherNumbers.reversed()
+    print("Reversed Array: \(reverseResult)")
 
-anotherNumbers.reverse()
-print("Reverse Array: \(anotherNumbers)")
+    anotherNumbers.reverse()
+    print("Reverse Array: \(anotherNumbers)")
+}
 
 //: [Next](@next)
