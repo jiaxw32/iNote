295dcdf48851655a5bfb615deb77264ac7337311
jiaxw32 <jiaxw32@gmail.com>
2022-10-18 12:03:37 +0800

    LLVM obfuscation test

--- HelloLLVM/HelloLLVM/main.m
+++ HelloLLVM/HelloLLVM/main.m
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <mach-o/dyld.h>
 #import <Foundation/Foundation.h>
 
 /*
@@ -39,6 +40,8 @@ void swap(int *a, int *b);
 int max(int a, int b) __attribute__((annotate("sub,indibr")));
 int main(void) __attribute__((annotate("strenc,indibr")));
 
+BOOL gs_checkEnv(void);
+
 int sum(int a, int b) {
     return a + b;
 }
@@ -58,6 +61,31 @@ int max(int a, int b){
     return a >= b ? a : b;
 }
 
+BOOL check_env(void) __attribute__((annotate("strenc, fco, indibr"))) {
+    char *env = getenv("DYLD_INSERT_LIBRARIES");
+    if (NULL != env) {
+        return YES;
+    }
+    return NO;
+}
+
+BOOL check_dylib(void) __attribute__((annotate("strenc, fco, indibr"))) {
+    int count = _dyld_image_count();
+    if (count > 0) {
+        for (int i = 0; i < count; i++) {
+            const char * dyld = _dyld_get_image_name(i);
+            if (strstr(dyld, "DynamicLibraries")) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+}
+
+BOOL combine_check(void) __attribute__((annotate("fco"))) {
+    return check_env() || check_dylib();
+}
+
 #pragma mark - MYClass
 
 @interface MYClass : NSObject
@@ -75,16 +103,78 @@ extern void hikari_nobcf(void);
 extern void hikari_indibr(void);
 extern void hikari_nodibr(void);
 
+extern void hikari_fco(void);
+extern void hikari_nofco(void);
+
 - (void)foo{
-    hikari_nobcf();
+//    hikari_nobcf();
     NSLog(@"call method: %s", __func__);
 }
 
 - (void)bar{
-    hikari_strenc();
-    hikari_indibr();
+//    hikari_strenc();
+//    hikari_indibr();
     NSLog(@"call method: %s", __func__);
 }
+
+// 混淆正常
++ (BOOL)checkEnv{
+    hikari_fco();
+    char *env = getenv("DYLD_INSERT_LIBRARIES");
+    if (NULL != env) {
+        return YES;
+    }
+    return NO;
+}
+
+// 混淆正常
++ (BOOL)checkDylib{
+    hikari_fco();
+    int count = _dyld_image_count();
+    for (int i = 0; i < count; i++) {
+        const char * name = _dyld_get_image_name(i);
+        if (strstr(name, "/Library/MobileSubstrate/DynamicLibraries")) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+// 混淆正常
++ (BOOL)test01{
+    return check_env();
+}
+
+// 混淆正常
++ (BOOL)test02{
+    return check_dylib();
+}
+
+// 混淆正常
++ (BOOL)test03{
+    return check_env() || check_dylib();
+}
+
+//+ (BOOL)test04{
+//    // 混淆报错
+////    return [self test01] || [self test02];
+//
+//    BOOL ret = [self test01];
+//    if (ret) {
+//        return ret;
+//    } else {
+//        return [self test02];
+//    }
+//}
+
+
+//+ (BOOL)test05{
+//    // 混淆报错
+////    return [self checkEnv] || [self checkDylib];
+//
+//    BOOL ret = [self checkEnv];
+//    return ret ?: [self checkDylib];
+//}
 
 @end
 
